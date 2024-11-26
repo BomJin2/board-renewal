1ea47834bb20aae10585106c385ca20bade94699
Bomjin2 <opjkl4006@naver.com>
2024-11-26 17:08:57 +0900

    localstorage에 유저 정보를 담아놓음

--- stores/atoms.ts
+++ stores/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 import { Todos, User } from "@/types";
-
+import { atomWithStorage } from "jotai/utils";
 
 /** Supabase에 저장되어 있는 'tasks' 테이블 내 모든 데이터 조회  */
 /** 전체 tasks 목록 상태 */
@@ -9,5 +9,4 @@ export const TodosAtom = atom<Todos[]>([]);
 /** 개별(단일) task 상태 */
 export const TodoAtom = atom<Todos | null>(null);
 
-export const userAtom = atom<User>();
-
+export const userAtom = atomWithStorage<User | null>("user", null);
