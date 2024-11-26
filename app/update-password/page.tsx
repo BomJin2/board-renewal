e30032de24d2d668386ce7ef2896d3d4f2b6e815
BomJin2 <89959092+BomJin2@users.noreply.github.com>
2024-11-26 16:46:18 +0900

    Feature/#5 login (#11)
    * 로그아웃 기능 구현
    
    * 비밀번호 재설정 기능 구현

--- app/update-password/page.tsx
+++ app/update-password/page.tsx
@@ -9,26 +9,17 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-function SignupPage() {
+function ChangePasswordPage() {
   const supabase = createClient();
   const { toast } = useToast();
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { checkEmail } = useEmailCheck();
+  const [aginPassword, setAgainPassword] = useState("");
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const togglePassword = () => setShowPassword((prevState) => !prevState);
+  const router = useRouter();
 
-  const handleSignup = async () => {
-    if (!email || !password) {
-      toast({
-        variant: "destructive",
-        title: "기입되지 않은 데이터(값)가 있습니다..",
-        description: "이메일과 비밀번호는 필수 값입니다.",
-      });
-      return;
-    }
-
-    if (password.length < 8) {
+  const handleChangePassword = async () => {
+    if (password.length && aginPassword.length < 8) {
       toast({
         variant: "destructive",
         title: "비밀버호는 최소 8자 이상이어야 합니다.",
@@ -36,46 +27,26 @@ function SignupPage() {
       });
       return;
     }
-
-    if (!checkEmail(email)) {
-      toast({
-        variant: "destructive",
-        title: "올바르지 않은 이메일 양식입니다.",
-        description: "올바른 이메일 양식을 작성해주세요!",
-      });
-      return;
-    }
     try {
-      const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-      });
-
-      console.log(data);
-      if (error) {
+      if (password === aginPassword) {
+        await supabase.auth.updateUser({ password: password });
         toast({
-          variant: "destructive",
-          title: "에러가 발생했습니다.",
-          description: `Supabase 오류: ${error.message || "알 수 없는 오류"}`,
-        });
-      } else if (data && !error) {
-        toast({
-          title: "회원가입을 성공하였습니다.",
-          description: "로그인 페이지로 이동하여 로그인을 진행해주세요",
+          title: "비밀번호가 변경되었습니다",
+          description: "새로운 비밀번호로 로그인해보세요",
         });
         router.push("/");
       }
-    } catch (e) {
-      toast({
-        variant: "destructive",
-        title: "네트워크 오류",
-        description: "서버와 연결할 수 없습니다. 다시 시도해주세요!",
-      });
-      console.error("API 호출 중 오류 발생:", e);
-    }
+
+      if (password !== aginPassword) {
+        toast({
+          title: "비밀번호를 다시 확인하세요",
+          description: "비밀번호를 재확인 해보세요",
+          variant: "destructive",
+        });
+      }
+    } catch (e) {}
   };
 
-  const router = useRouter();
   return (
     <div className="page">
       <div className="page__container">
@@ -93,31 +64,37 @@ function SignupPage() {
 
         <Card className="w-[400px]">
           <CardHeader className="space-y-1">
-            <CardTitle className="text-2xl">회원가입</CardTitle>
-            <CardDescription>계정을 생성하기 위해 아래 정보를 입력해주세요.</CardDescription>
+            <CardTitle className="text-2xl">비밀번호 변경</CardTitle>
+            <CardDescription>비밀번호 변경을 위해 변경된 비밀번호를 입력해주세요.</CardDescription>
           </CardHeader>
           <CardContent className="grid gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">이메일</Label>
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                placeholder="이메일을 입력하세요."
-                required
-                onChange={(e) => setEmail(e.target.value)}
-                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-              />
-            </div>
             <div className="relative grid gap-2">
-              <Label htmlFor="password">비밀번호</Label>
+              <Label htmlFor="email">비밀번호</Label>
               <Input
                 id="password"
                 type={showPassword ? "text" : "password"}
                 placeholder="비밀번호를 입력하세요."
                 required
                 value={password}
-                onChange={(event) => setPassword(event.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                size={"icon"}
+                className="absolute top-[38px] right-2 -translate-y-1/4 bg-transparent hover:bg-transparent"
+                onClick={togglePassword}
+              >
+                {showPassword ? <EyeOff className="h-5 w-5 text-muted-foreground" /> : <Eye className="h-5 w-5 text-muted-foreground" />}
+              </Button>
+            </div>
+            <div className="relative grid gap-2">
+              <Label htmlFor="againPassword">비밀번호 재확인</Label>
+              <Input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="비밀번호를 입력하세요."
+                required
+                value={aginPassword}
+                onChange={(e) => setAgainPassword(e.target.value)}
               />
               <Button
                 size={"icon"}
@@ -133,7 +110,7 @@ function SignupPage() {
               <span className="w-full border-t"></span>
             </div>
             <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">간편 회원가입을 원하시면 이전 버튼을 누르세요</span>
+              <span className="bg-background px-2 text-muted-foreground"></span>
             </div>
           </div>
           <CardFooter className="flex flex-col mt-6">
@@ -143,9 +120,9 @@ function SignupPage() {
               </Button>
               <Button
                 className="w-full text-white bg-[#E79057] hover:bg-[#E26F24] hover:ring-1 hover:ring-[#E26F24] hover:ring-offset-1 active:bg-[#D5753D] hover:shadow-lg"
-                onClick={handleSignup}
+                onClick={handleChangePassword}
               >
-                회원가입
+                비밀번호 변경
               </Button>
             </div>
 
@@ -162,4 +139,4 @@ function SignupPage() {
   );
 }
 
-export default SignupPage;
+export default ChangePasswordPage;
